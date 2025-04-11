8571abde017952d743220379f79f7970de831cf9
NereoAyala <167773112+NereoAyala@users.noreply.github.com>
2025-04-11 11:22:22 -0300

    commit

--- Banco/src/main/java/dto/Cuenta.java
+++ Banco/src/main/java/dto/Cuenta.java
@@ -6,16 +6,7 @@ public abstract class Cuenta {
     public Cuenta(double saldoInicial) {
         this.saldo = saldoInicial;
     }
-
-    protected synchronized boolean operar(double monto) {
-        saldo += monto;
-        return true;
-    }
-
-    protected synchronized boolean puedeQuitar(double monto) {
-        return saldo >= monto;
-    }
-
+    
     public synchronized double obtenerSaldo() {
         return saldo;
     }
