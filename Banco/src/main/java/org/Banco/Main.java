6e39a8394eda2bba63c6f3de8867f3a4b89cdd1e
NereoAyala <167773112+NereoAyala@users.noreply.github.com>
2025-04-24 16:09:49 -0300

    Logica y Main

--- Banco/src/main/java/org/Banco/Main.java
+++ Banco/src/main/java/org/Banco/Main.java
@@ -1,7 +1,67 @@
 package org.Banco;
 
+import org.Banco.dto.CajaDeAhorro;
+import org.Banco.dto.Cuenta;
+import org.Banco.dto.CuentaCorriente;
+import org.Banco.services.LogicaCuenta;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
     public static void main(String[] args) {
+        LogicaCuenta logica = LogicaCuenta.getInstance();
+        Random rand=new Random();
+        int index = rand.nextInt(10);
 
+        for (int i = 0; i < 10; i++) {
+            if (rand.nextBoolean()) {
+                logica.agregarCuenta(new CajaDeAhorro.Builder()
+                        .whitSaldoInicial(0).build());
+            } else {
+                double giro = rand.nextDouble() * 500;
+                logica.agregarCuenta(new CuentaCorriente.Builder()
+                        .whitSaldoInicial(0).whitGiroDescubierto(0).build());
+            }
+        }
+
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+
+        for (int i = 0; i < 10.000; i++) {
+            executor.submit(() -> {
+                int cuentaId = rand.nextInt(10);
+                double monto = rand.nextDouble() * 100;
+                boolean agregar = rand.nextBoolean();
+
+                if (agregar) {
+                    logica.agregarSaldo(cuentaId, monto);
+                } else {
+                    logica.quitarSaldo(cuentaId, monto);
+                }
+            });
+        }
+        executor.shutdown();
+        while (!executor.isTerminated()) {
+        }
+
+        System.out.println("---- RESULTADOS ----");
+        for (int i = 0; i < 10; i++) {
+            Cuenta cuenta = logica.getCuenta(i);
+
+            int operaciones = 0;
+
+            if (cuenta instanceof CajaDeAhorro) {
+                operaciones = ((CajaDeAhorro) cuenta).getOperaciones();
+            } else if (cuenta instanceof CuentaCorriente) {
+                operaciones = ((CuentaCorriente) cuenta).getOperaciones();
+            }
+
+            System.out.printf("Cuenta %d (%s): Saldo final = %.2f | Movimientos = %d%n",
+                    i,
+                    cuenta.getClass().getSimpleName(),
+                    cuenta.getSaldo(),
+                    operaciones);
+        }
     }
 }
---
+++ Banco/src/main/java/org/Banco/services/LogicaCuenta.java
@@ -0,0 +1,87 @@
+package org.Banco.services;
+
+import org.Banco.dto.CajaDeAhorro;
+import org.Banco.dto.Cuenta;
+import org.Banco.dto.CuentaCorriente;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class LogicaCuenta {
+    private static volatile LogicaCuenta instance;
+    public List<Cuenta>Cuentas=new ArrayList<>();
+
+    private LogicaCuenta() {}
+
+    public static LogicaCuenta getInstance() {
+        if (instance == null) {
+            synchronized (LogicaCuenta.class) {
+                if (instance == null) {
+                    instance = new LogicaCuenta();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public void agregarCuenta(Cuenta cuenta)
+    {
+        Cuentas.add(cuenta);
+    }
+
+    private boolean esIndiceValido(int i) {
+        return i >= 0 && i < Cuentas.size();
+    }
+
+    public boolean agregarSaldo(int indice,double monto)
+    {
+        if (esIndiceValido(indice)) {
+            Cuenta cuenta=Cuentas.get(indice);
+            if (cuenta instanceof CajaDeAhorro caja) {
+                return caja.agregarSaldo(monto);
+            } else if (cuenta instanceof CuentaCorriente c) {
+                return c.agregarSaldo(monto);
+            }
+        }
+        return false;
+    }
+
+    public boolean quitarSaldo(int indice,double monto)
+    {
+        if (esIndiceValido(indice)) {
+            Cuenta cuenta=Cuentas.get(indice);
+            if (cuenta instanceof CajaDeAhorro caja) {
+                return caja.quitarSaldo(monto);
+            } else if (cuenta instanceof CuentaCorriente c) {
+                return c.quitarSaldo(monto);
+            }
+        }
+        return false;
+    }
+
+    public double consultarSaldo(int indice)
+    {
+        if (esIndiceValido(indice)) {
+            Cuenta cuenta=Cuentas.get(indice);
+            return cuenta.getSaldo();
+        }
+        return -1;
+    }
+
+    public int cantidadCuentas()
+    {
+        return Cuentas.size();
+    }
+
+    public Cuenta getCuenta(int indice)
+    {
+        if (esIndiceValido(indice)) {
+            return Cuentas.get(indice);
+        } else{
+            System.out.println("Indice no Valido: "+indice);
+            return null;
+        }
+    }
+}
+
